ae80496977e82399e1e9dc590181ebfdb33139ff
SergeyMotrokhov <leshch2016@yandex.ru>
2020-08-26 23:23:12 +0300

    added all

---
+++ src/test/java/Employee.java
@@ -0,0 +1,32 @@
+public class Employee {
+    String fio;
+    int salary;
+    int age;
+
+    public Employee() {
+    }
+
+    public Employee(String fio, int salary, int age) {
+        this.fio = fio;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void addSalary(int salary, int age, int k, int b) {
+        if (age > k) {
+            this.salary += b;
+        }
+    }
+}
---
+++ src/test/java/MainClass.java
@@ -0,0 +1,22 @@
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        Employee[] employee = new Employee[5];
+        employee[0] = new Employee("Dralin M.M.", 30000, 35);
+        employee[1] = new Employee("Inanov I.I.", 35000, 29);
+        employee[2] = new Employee("Petrov A.B.", 40000, 40);
+        employee[3] = new Employee("Mamaev A.B.", 45000, 41);
+        employee[4] = new Employee("Egorov E.E.", 50000, 45);
+
+        for (Employee value : employee) {
+            if (value.salary > 40000) {
+                System.out.println("ФИО: " + value.fio);
+                System.out.println("Salary: " + value.salary);
+                System.out.println("Age: " + value.age);
+            }
+        }
+
+        System.out.println("Hello!!!");
+    }
+}
